344de892a12092a488e806f21d8da4a20acc385c
ArmanO2005 <cyclanthaceae@berkeley.edu>
2024-12-26 19:38:21 -0800

    NFL season records added

--- ESPN_Scraper.py
+++ ESPN_Scraper.py
@@ -11,8 +11,49 @@ headers = {
                    "Chrome/108.0.0.0 Safari/537.36")
 }
 
+def NFL_Get_Scores(season=2024):
+    titles = ['awayTeam', 'homeTeam', 'awayScore', 'homeScore']
 
-def All_Player_Stats():
+    scores = []
+    w = 1
+
+    rows = []
+    while True:
+        url = (f"https://www.espn.com/nfl/scoreboard/_/week/{w}/year/{season}/seasontype/2")
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            print(response.status_code)
+            break
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        html = soup.prettify()
+
+        if html.count("No games on this date.") > 1:
+            break
+
+        scores = re.findall(r'<div class="ScoreCell__Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2">(.*?)<\/div>', html, flags=re.DOTALL)
+        names = re.findall(r'<div class="ScoreCell__TeamName ScoreCell__TeamName--shortDisplayName db">(.*?)<\/div>', html, flags=re.DOTALL)
+
+        for i in range(int(len(scores)/2)):
+            row = []
+            row.append(names[i * 2])
+            row.append(names[i * 2 + 1])
+            row.append(scores[i * 2])
+            row.append(scores[i * 2 + 1])
+
+            for i in range(len(row)):
+                row[i] = row[i].strip()
+            
+            rows.append(row)
+
+        
+        w += 1
+
+    return pd.DataFrame(rows, columns=titles)
+
+
+def NFL_All_Player_Stats():
     url = (f"https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=false&page=0&limit=10")
     total = requests.get(url, headers=headers)
 
@@ -172,18 +213,10 @@ def NFL_Turnovers():
 
 
 
-
-
-
-
-
-# All_Player_Stats().to_csv("All_Player_Stats.csv", index=False)
-
-# with open("output.json", 'w') as f:
-#     json.dump(stats, f, indent=4)
-
-# with open("player_example.json", 'w') as f:
-#     json.dump(stats['athletes'][3], f, indent=4)
-
+# x = requests.get("https://www.espn.com/nfl/scoreboard/_/week/22/year/2024/seasontype/2", headers=headers)
+# soup = BeautifulSoup(x.text, "html.parser")
 # with open("output.txt", 'w', encoding="utf-8") as f:
-#     f.write(str(soup.prettify()), )
+#     f.write(str(soup.prettify()))
+
+
+print(NFL_Get_Scores().head())
